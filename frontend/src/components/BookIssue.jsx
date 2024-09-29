1e600dad18ae93952d4fd2e94bbef222d545be1e
Sanjana Acharya <sensanjana072@gmail.com>
2024-09-29 21:38:33 +0530

    book issue and return added

---
+++ frontend/src/components/BookIssue.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import api from '../api/axiosConfig'
+
+const BookIssue = () => {
+
+    const [bookName, setBookName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const [responseMsg, setResponseMsg] = useState('');
+
+    const issueBook = async(e) => {
+        e.preventDefault()
+        try {
+            const response = await api.post('/api/transactions/issue', { bookName, email });
+            setResponseMsg(response.data.message)
+            setBookName('')
+            setEmail('')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className='w-[90%] mx-auto border shadow p-3 mt-5'>
+            <h1 className='font-semibold text-xl text-green-800 '>Issue a Book</h1>
+            <p className='text-gray-500 text-sm'>Issue a Book to a User</p>
+            <form onSubmit={issueBook} className='mt-3 w-[50%] p-4'>
+                <div className='flex justify-between mb-2'>
+                    <label>Book Name: </label>
+                    <input type="text" value={bookName} onChange={(e) => setBookName(e.target.value)} className='border' required />
+                </div>
+                
+                <div className='flex justify-between'>
+                <label>Email: </label>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className='border' required />
+               
+                </div>
+                <button type="submit" className='text-white bg-green-800 p-1 text-sm mt-3'>Issue Book</button>
+                <p className='mt-2 text-red-500'> {responseMsg}</p>
+            </form>
+        </div>
+    )
+}
+
+export default BookIssue
--- frontend/src/components/Transaction.jsx
+++ frontend/src/components/Transaction.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import BookIssue from './BookIssue'
+import BookReturn from './BookReturn'
 
 const Transaction = () => {
   return (
     <div>
         <Header/>
-        trans
+        <BookIssue/>
+        <BookReturn/>
     </div>
   )
 }
