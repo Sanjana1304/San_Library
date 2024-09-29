057f38f989e492ef3c5f5155be7c3b57714d4bd3
Sanjana Acharya <sensanjana072@gmail.com>
2024-09-29 19:51:54 +0530

    header done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navig = useNavigate();
+
+  const openAbout = () => {
+    navig('/transaction');
+  }
+
+  const openHome = () => {
+    navig('/');
+  }
+
   return (
-    <div>Header</div>
+   <nav className='font-semibold flex justify-between p-5 text-green-800 text-lg'>
+    <h1>San's Library</h1>
+    <ul className='flex justify-between cursor-pointer'>
+        <li className='mr-6' onClick={openHome}>Home</li>
+        <li className='mr-6' onClick={openAbout}>Transactions</li>
+    </ul>
+   </nav>
   )
 }
 
