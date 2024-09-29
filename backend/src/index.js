c578de02f05214fdc46213081bbf344d896f8dfd
Sanjana Acharya <sensanjana072@gmail.com>
2024-09-29 15:01:24 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(
+  cors({
+      origin : process.env.FRONTEND_URL,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials : true,
+  })
+)
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log('MongoDB connected successfully hehe');
+})
+.catch(err => {
+  console.error('Ayo Error connecting to MongoDB:', err);
+});
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, Sanj from the backend !!!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Woohoo! Server listening on port ${port}`);
+});
