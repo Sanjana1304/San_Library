36b4e485d70e406f339280b513f20722d208906c
Sanjana Acharya <sensanjana072@gmail.com>
2024-09-30 14:11:32 +0530

    modified backend url

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://san-library.onrender.com/',
     withCredentials: true, // Include cookies in requests
     headers: {
         'Content-Type': 'application/json'
